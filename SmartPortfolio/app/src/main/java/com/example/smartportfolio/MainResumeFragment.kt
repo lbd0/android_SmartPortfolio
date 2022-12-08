e0cde0a3d4fe93a8e1f3290dcfa3108a0e9064e6
BADA LEE <dlqkek8@naver.com>
2022-12-08 23:55:47 +0900

    메인-레쥬메 진행중

--- SmartPortfolio/app/src/main/java/com/example/smartportfolio/MainResumeFragment.kt
+++ SmartPortfolio/app/src/main/java/com/example/smartportfolio/MainResumeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.smartportfolio.databinding.FragmentMainResumeBinding
 
 // 이력 화면
 class MainResumeFragment : Fragment() {
@@ -15,6 +16,12 @@ class MainResumeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_resume, container, false)
+
+        val binding = FragmentMainResumeBinding.inflate(layoutInflater)
+
+        val contents = mutableListOf<String>("동화고등학교 졸업", "한림대학교 입학")
+        val contents_sub = mutableListOf<String>("2020.1.26", "2020.3.1")
     }
+
 
 }
